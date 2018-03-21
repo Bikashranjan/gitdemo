ccd8931950bbe6dec9942be11cad03ecad240cc6
Bikash Ranjan Bhuyan <Bikash.Bhuyan@ADRAPIDVALUE.COM>
2018-03-21 16:49:57 +0530

    first commit

---
+++ GitDeMo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitDeMo.Startup))]
+namespace GitDeMo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
